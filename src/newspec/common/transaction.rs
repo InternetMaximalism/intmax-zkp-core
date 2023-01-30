ddc57d1bc4fb0d585d343b6ac53cac36ab45bd47
nuno <41233686+qope@users.noreply.github.com>
2023-01-31 00:11:23 +0900

    impl hash to transaction

--- src/newspec/common/transaction.rs
+++ src/newspec/common/transaction.rs
@@ -1,6 +1,6 @@
 use super::{account::Address, asset::TokenKind};
 use num::BigUint;
-use plonky2::hash::hash_types::RichField;
+use plonky2::hash::hash_types::{HashOut, RichField};
 
 /// Transaction which specifies a reciever, a token kind, and an amount.
 /// `amount` should be below `MAX_AMOUNT`
@@ -8,4 +8,13 @@ pub struct Transaction<F: RichField> {
     pub to: Address<F>,
     pub kind: TokenKind<F>,
     pub amount: BigUint,
+}
+
+impl<F: RichField> Transaction<F> {
+    pub fn default(&self) -> Self {
+        todo!()
+    }
+    pub fn hash(&self, _nullifier: HashOut<F>) -> HashOut<F> {
+        todo!()
+    }
 }

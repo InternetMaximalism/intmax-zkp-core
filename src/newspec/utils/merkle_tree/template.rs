8a9c37f5599d1e2bdd36427933ed2b225a47c843
hrmk1o3 <20158457+hrmk1o3@users.noreply.github.com>
2023-02-01 15:48:54 +0400

    Add merkle_tree_template

---
+++ src/newspec/utils/merkle_tree/template.rs
@@ -0,0 +1,34 @@
+use std::marker::PhantomData;
+
+use plonky2::{hash::hash_types::RichField, plonk::config::Hasher};
+
+use crate::newspec::common::traits::Leafable;
+
+pub trait NodeData<F: RichField, H: Hasher<F>, V: Leafable<F, H>> {
+    fn new() -> Self;
+    fn get_inner_hash(&self, path: &[bool]) -> H::Hash;
+    fn get_leaf_data(&self, index: usize) -> V;
+    fn insert_inner_hash(&mut self, path: Vec<bool>, value: H::Hash);
+    fn insert_leaf_data(&mut self, index: usize, value: H::Hash);
+}
+
+/// Sparse Merkle Tree which is compatible to the native plonky2 Merkle Tree.
+#[derive(Debug)]
+pub struct MerkleTreeTemplate<F: RichField, H: Hasher<F>, V: Leafable<F, H>, N: NodeData<F, H, V>> {
+    height: usize,
+    storage: N,
+    zero_hashes: Vec<H::Hash>,
+    _value: PhantomData<V>,
+}
+
+// pub struct NodeDataMemory {
+//     node_hashes: HashMap<Vec<bool>, H::Hash>,
+//     leaves: HashMap<usize, V>,
+// }
+
+// impl NodeData for NodeDataMemory {
+//     // ...
+// }
+
+// type MerkleTreeMemory<F: RichField, H: Hasher<F>, V: Leafable<F, H>> =
+//     MerkleTreeTemplate<F, H, V, NodeDataMemory>;

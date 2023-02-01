6a2d4e0d30506e05300cb0d825609f9eaeb6527b
hrmk1o3 <20158457+hrmk1o3@users.noreply.github.com>
2023-02-01 15:52:34 +0400

    Add userAssetTree

--- src/newspec/client/user_asset_tree.rs
+++ src/newspec/client/user_asset_tree.rs
@@ -1,1 +1,14 @@
-//! Write the definition of the user asset tree
+use std::collections::HashMap;
+
+use plonky2::{hash::hash_types::RichField, plonk::config::Hasher};
+
+use crate::newspec::{
+    common::asset::{Asset, TokenKind},
+    utils::merkle_tree::merkle_tree::MerkleTree,
+};
+
+pub struct UserAssetTree<F: RichField, H: Hasher<F>> {
+    pub merkle_tree: MerkleTree<F, H, Asset<F>>,
+    pub token_index_map: HashMap<TokenKind<F>, usize>,
+    max_token_index: usize,
+}

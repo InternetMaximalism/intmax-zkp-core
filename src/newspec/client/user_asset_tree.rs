bfa2e4133f4b24d020fe60e62843f98ea01a0088
hrmk1o3 <20158457+hrmk1o3@users.noreply.github.com>
2023-02-04 20:17:07 +0400

    rename token_index to asset_id

--- src/newspec/client/user_asset_tree.rs
+++ src/newspec/client/user_asset_tree.rs
@@ -8,17 +8,17 @@ pub struct UserAssetTree<F: RichField, H: Hasher<F>> {
     pub merkle_tree: MerkleTree<F, H, Asset>,
 
     /// asset_id -> leaf_index
-    pub token_index_map: HashMap<usize, usize>,
+    pub asset_id_map: HashMap<usize, usize>,
 
-    max_token_index: usize,
+    max_leaf_index: usize,
 }
 
 impl<F: RichField, H: Hasher<F>> UserAssetTree<F, H> {
     pub fn new(height: usize) -> Self {
         Self {
             merkle_tree: MerkleTree::new(height),
-            token_index_map: HashMap::new(),
-            max_token_index: 0,
+            asset_id_map: HashMap::new(),
+            max_leaf_index: 0,
         }
     }
 }

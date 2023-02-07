c310ff89f1cc82ea1a297b3b7a7b60611e5dbc6d
hrmk1o3 <20158457+hrmk1o3@users.noreply.github.com>
2023-02-07 14:42:56 +0400

    Implement helper circuits

--- src/another_spec/common/asset.rs
+++ src/another_spec/common/asset.rs
@@ -42,3 +42,40 @@ impl AddAssign for Assets {
         todo!()
     }
 }
+
+/// Returns `hash_x_with_salt`
+pub fn verify_amount_hash<F: RichField, H: Hasher<F>>(
+    _x: &Assets,
+    /* private */ _salt: [F; 4],
+) -> anyhow::Result<H::Hash> {
+    // anyhow::ensure!(hash(salt, x) == hash_x_with_salt);
+    todo!()
+}
+
+/// Returns `(hash_x, hash_y, hash_z)`
+pub fn add_amounts<F: RichField, H: Hasher<F>>(
+    /* private */ x: Assets,
+    /* private */ y: Assets,
+    /* private */ z: Assets,
+) -> anyhow::Result<(H::Hash, H::Hash, H::Hash)> {
+    let hash_x = x.hash::<H>();
+    let hash_y = y.hash::<H>();
+    let hash_z = z.hash::<H>();
+    anyhow::ensure!(x + y == z);
+
+    Ok((hash_x, hash_y, hash_z))
+}
+
+/// Returns `(hash_x, hash_y)`
+pub fn is_greater_than<F: RichField, H: Hasher<F>>(
+    /* private */ x: Assets,
+    /* private */ y: Assets,
+) -> anyhow::Result<(H::Hash, H::Hash)> {
+    let hash_x = x.hash::<H>();
+    let hash_y = y.hash::<H>();
+
+    // For multi-asset balances this means that the balance is positive for all assets.
+    anyhow::ensure!(x >= y);
+
+    Ok((hash_x, hash_y))
+}
--- src/another_spec/utils/signature.rs
+++ src/another_spec/utils/signature.rs
@@ -1,2 +1,35 @@
+use plonky2::{
+    hash::hash_types::{HashOut, RichField},
+    plonk::config::Hasher,
+};
+
+use crate::{
+    another_spec::common::transaction::TransferBatch,
+    newspec::common::{account::Address, traits::Leafable},
+};
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct BlsSignature(pub Vec<u8>);
+
+/// Returns `transfer_batch_hash`
+pub fn block_signature_is_valid<F: RichField, H: Hasher<F>>(
+    /* private */ batch: &TransferBatch<F>,
+) -> anyhow::Result<H::Hash> {
+    let transfer_batch_hash = batch.hash::<H>();
+
+    verify_bls_signature(
+        batch.transaction_tree_root,
+        batch.signature.clone(),
+        &batch.sender_list,
+    )?;
+
+    Ok(transfer_batch_hash)
+}
+
+pub fn verify_bls_signature<F: RichField>(
+    _message: HashOut<F>,
+    _signature: BlsSignature,
+    _keys: &[Address],
+) -> anyhow::Result<()> {
+    todo!()
+}

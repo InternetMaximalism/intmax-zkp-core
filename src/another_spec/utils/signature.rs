a0438556954b3f7c3a1a9bc87ea52f694e1aa850
nuno <41233686+qope@users.noreply.github.com>
2023-02-12 21:06:31 +0900

    add test_bls_signature

--- src/another_spec/utils/signature.rs
+++ src/another_spec/utils/signature.rs
@@ -54,3 +54,43 @@ pub fn verify_bls_signature_target<F: RichField + Extendable<D>, const D: usize>
 ) {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use plonky2::{
+        field::{goldilocks_field::GoldilocksField, types::Field},
+        hash::poseidon::PoseidonHash,
+        plonk::config::Hasher,
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_bls_signature() {
+        type F = GoldilocksField;
+        let secret_key_1 =
+            hex::decode("c9afa9d845ba75166b5c215767b1d6934e50c3db36e89b127b8a622b120f6721")
+                .unwrap();
+
+        let secret_key_2 =
+            hex::decode("a55e93edb1350916bf5beea1b13d8f198ef410033445bcb645b65be5432722f1")
+                .unwrap();
+
+        let public_key_1 = Bn256.derive_public_key(&secret_key_1).unwrap();
+        let public_key_2 = Bn256.derive_public_key(&secret_key_2).unwrap();
+
+        let hashed_message = PoseidonHash::hash_no_pad(&[F::ONE, F::TWO]);
+
+        let sig_1 = Bn256
+            .sign(&secret_key_1, &hashed_message.to_bytes())
+            .unwrap();
+        let sig_2 = Bn256
+            .sign(&secret_key_2, &hashed_message.to_bytes())
+            .unwrap();
+
+        let agg_sig = Bn256.aggregate_signatures(&[&sig_1, &sig_2]).unwrap();
+
+        let keys = &[PublicKey(public_key_1), PublicKey(public_key_2)];
+        assert!(verify_bls_signature(hashed_message, BlsSignature(agg_sig), keys).is_ok());
+    }
+}

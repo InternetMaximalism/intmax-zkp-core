f1fc0543706cfdd5efc82ac8cf9ad8b7a92bd957
nuno <41233686+qope@users.noreply.github.com>
2023-02-12 16:18:00 +0900

    impl bls

--- src/another_spec/utils/signature.rs
+++ src/another_spec/utils/signature.rs
@@ -1,21 +1,25 @@
+use anyhow::Ok;
 use plonky2::{
     field::extension::Extendable,
     hash::hash_types::{HashOut, HashOutTarget, RichField},
     iop::{target::BoolTarget, witness::Witness},
-    plonk::circuit_builder::CircuitBuilder,
+    plonk::{circuit_builder::CircuitBuilder, config::GenericHashOut},
 };
 
-use crate::newspec::common::account::{Address, AddressTarget};
+use crate::newspec::common::account::{Address, AddressTarget, PrivateKey};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct BlsSignature(pub Vec<u8>);
 
 pub fn verify_bls_signature<F: RichField>(
-    _hashed_message: HashOut<F>,
-    _signature: BlsSignature,
-    _keys: &[Address],
+    hashed_message: HashOut<F>,
+    signature: BlsSignature,
+    keys: &[PrivateKey],
 ) -> anyhow::Result<()> {
-    todo!()
+    let hashed_message = hashed_message.to_bytes();
+    let agg_pug_key = Bn256.aggregate_public_keys(keys.map(|key| key.0).collect());
+    Bn256.verify(signature.0, hashed_message, agg_pub_key)?;
+    Ok(())
 }
 
 #[derive(Clone, Debug)]
